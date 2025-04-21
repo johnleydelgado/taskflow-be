ba0a0f826fa9ab78a451c3b344ba856fcf715b77
johnleydelgado <johnley00@gmail.com>
2025-04-21 19:57:44 +0800

    changes in index

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,19 @@
-import express from 'express';
-const app = express();
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import 'dotenv/config';
+import server from '../src/server';
+import { connectDB } from '../src/db';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-app.get('/', (req: express.Request, res: express.Response) =>
-{
-  res.send('Express on Vercel');
-},
-);
+let connected = false;
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
-
-module.exports = app;
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse,
+) {
+  if (!connected) {
+    await connectDB();       // ensure DB is ready
+    connected = true;
+  }
+  // hand off to your Express “server” instance
+  return server(req, res);
+}
